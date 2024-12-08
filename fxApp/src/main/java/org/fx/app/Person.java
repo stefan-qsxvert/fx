653c48dec33e7ab99f7660d378733caae6f06dc4
tee <tee@inspirion>
2024-12-08 22:44:00 +0100

    1

--- fxApp/src/main/java/org/fx/app/Person.java
+++ fxApp/src/main/java/org/fx/app/Person.java
@@ -4,9 +4,9 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class Person {
 	 
-    private final SimpleStringProperty firstName;
-    private final SimpleStringProperty lastName;
-    private final SimpleStringProperty email;
+    private  SimpleStringProperty firstName;
+    private  SimpleStringProperty lastName;
+    private  SimpleStringProperty email;
 
     public Person(String fName, String lName, String email) {
         this.firstName = new SimpleStringProperty(fName);
@@ -14,27 +14,53 @@ public class Person {
         this.email = new SimpleStringProperty(email);
     }
 
-    public String getFirstName() {
-        return firstName.get();
-    }
+	public SimpleStringProperty getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String fName) {
-        firstName.set(fName);
-    }
+	public void setFirstName(SimpleStringProperty firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName.get();
-    }
+	public SimpleStringProperty getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String fName) {
-        lastName.set(fName);
-    }
+	public void setLastName(SimpleStringProperty lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getEmail() {
-        return email.get();
-    }
+	public SimpleStringProperty getEmail() {
+		return email;
+	}
 
-    public void setEmail(String fName) {
-        email.set(fName);
-    }
+	public void setEmail(SimpleStringProperty email) {
+		this.email = email;
+	}
+
+    
+	
+//    public String getFirstName() {
+//        return firstName.get();
+//    }
+//
+//    public void setFirstName(String fName) {
+//        firstName.set(fName);
+//    }
+//
+//    public String getLastName() {
+//        return lastName.get();
+//    }
+//
+//    public void setLastName(String fName) {
+//        lastName.set(fName);
+//    }
+//
+//    public String getEmail() {
+//        return email.get();
+//    }
+//
+//    public void setEmail(String fName) {
+//        email.set(fName);
+//    }
 }
