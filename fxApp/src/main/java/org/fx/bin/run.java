e87fb95fd5f406168a6398d3e10749f90030053f
tee <tee@inspirion>
2024-12-08 16:26:46 +0100

    1

--- fxApp/src/main/java/org/fx/bin/run.java
+++ fxApp/src/main/java/org/fx/bin/run.java
@@ -1,6 +1,11 @@
 package org.fx.bin;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class run extends Application{
@@ -12,6 +17,18 @@ public class run extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
+		Button button = new Button("klik!");
+		TextField texf = new TextField();
+		
+		Group group = new Group(button);
+//		gr(texf);
+		Scene scene = new Scene(group);
+
+		
+		
+		primaryStage.setScene(scene);
+		primaryStage.setHeight(480);
+		primaryStage.setWidth(640);
 		primaryStage.show();
 	}
 }
