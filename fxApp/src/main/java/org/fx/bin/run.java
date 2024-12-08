1bc2e5b3bd1a7ec7d66f0ca122f3921badc5d18b
tee <tee@inspirion>
2024-12-08 20:49:56 +0100

    1

--- fxApp/src/main/java/org/fx/bin/run.java
+++ fxApp/src/main/java/org/fx/bin/run.java
@@ -10,6 +10,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
@@ -22,14 +26,20 @@ public class run extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		Label label = new Label("Address Book");
-		label.setPrefWidth(360);
+		label.setMinWidth(360);
+		label.setMaxWidth(360);
+		label.minHeight(16);
+		label.setFont(new Font("Arial", 14));
+		label.setBorder(Border.stroke(null));;
+		label.setLayoutX(10);
+		label.setLayoutY(10);
 		label.setTextAlignment(TextAlignment.CENTER);
 
 		
 		Button button = new Button("klik!");
 		TextField texf = new TextField();
-		texf.setLayoutX(140);
-		texf.setLayoutY(24);
+		texf.setLayoutX(380);
+		texf.setLayoutY(36);
 		
 		Group group = new Group();
 		
@@ -38,6 +48,9 @@ public class run extends Application{
 		TableView table = new TableView();
 		table.setMinWidth(360);
 		table.setMaxWidth(360);
+		table.setEditable(true);
+		table.setLayoutX(10);
+		table.setLayoutY(36);
 		
 		TableColumn coll0 = new TableColumn("Kolumna 1");
 		TableColumn coll1 = new TableColumn("Kolumna 1");
@@ -47,15 +60,15 @@ public class run extends Application{
 		coll1.setPrefWidth(120);
 		coll2.setPrefWidth(120);
 		
+		coll0.setCellValueFactory(new PropertyValueFactory<String, String>("Id"));
+		coll1.setCellValueFactory(new PropertyValueFactory<String, String>("Id1"));
+		coll2.setCellValueFactory(new PropertyValueFactory<String, String>("Id2"));
+		
 		coll0.setCellFactory(null); //setText("123");
 		
 		table.getColumns().add(coll0);
 		table.getColumns().add(coll1);
 		table.getColumns().add(coll2);
-		
-		table.setEditable(true);
-		table.setLayoutX(10);
-		table.setLayoutY(36);
 		
 		group.getChildren().addAll(table, label);
 		
