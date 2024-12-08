dd3a1bd1d9c6915178f21f853618da0b9ef2a985
tee <tee@inspirion>
2024-12-08 20:25:09 +0100

    1

--- fxApp/src/main/java/org/fx/bin/run.java
+++ fxApp/src/main/java/org/fx/bin/run.java
@@ -1,15 +1,21 @@
 package org.fx.bin;
 
+import javax.swing.GroupLayout.Alignment;
+
 // --module-path /home/tee/git/lib/ --add-modules javafx.controls,javafx.fxml
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class run extends Application{
@@ -20,22 +26,45 @@ public class run extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		Label label = new Label("Address Book");
+		label.setPrefWidth(360);
+		label.setTextAlignment(TextAlignment.CENTER);
+
 		
 		Button button = new Button("klik!");
 		TextField texf = new TextField();
 		texf.setLayoutX(140);
 		texf.setLayoutY(24);
-		Group group = new Group(button);
+		
+		Group group = new Group();
+		
 		group.getChildren().add(texf);
-		Scene scene = new Scene(group);
 		
 		TableView table = new TableView();
+		table.setMinWidth(360);
+		table.setMaxWidth(360);
 		
 		TableColumn coll0 = new TableColumn("Kolumna 1");
+		TableColumn coll1 = new TableColumn("Kolumna 1");
+		TableColumn coll2 = new TableColumn("Kolumna 1");
+		
+		coll0.setPrefWidth(120);
+		coll1.setPrefWidth(120);
+		coll2.setPrefWidth(120);
+		
+		coll0.setCellFactory(null); //setText("123");
+		
 		table.getColumns().add(coll0);
+		table.getColumns().add(coll1);
+		table.getColumns().add(coll2);
 		
 		table.setEditable(true);
-		group.getChildren().add(table);
+		table.setLayoutX(10);
+		table.setLayoutY(36);
+		
+		group.getChildren().addAll(table, label);
+		
+		Scene scene = new Scene(group);
 		
 		primaryStage.setScene(scene);
 		primaryStage.setHeight(480);
