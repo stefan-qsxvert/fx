183df290498491f4cb405ae16844bc7ad7d45ff9
tee <tee@inspirion>
2024-12-08 16:13:37 +0100

    1

---
+++ fxApp/src/main/java/org/fx/bin/run.java
@@ -0,0 +1,17 @@
+package org.fx.bin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class run extends Application{
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		primaryStage.show();
+	}
+}
