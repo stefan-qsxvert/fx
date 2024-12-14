f0bf4f70e08e97b3ff43073deef9eed68bee6bca
tee <tee@inspirion>
2024-12-15 00:07:25 +0100

    1

--- fxApp/src/main/java/org/bin/run.java
+++ fxApp/src/main/java/org/bin/run.java
@@ -7,8 +7,8 @@ import org.app.EdekTest;
 public class run{
 	
 	public static void main(String[] args) {
-		Gui gui = new Gui();
-//		gui.start();
+//		Gui gui = new Gui();
+////		gui.start();
 		EdekTest edekTest = new EdekTest();
 		edekTest.run();
 		
