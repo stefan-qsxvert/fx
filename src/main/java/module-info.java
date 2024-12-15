cfbd8ef628f2e594c70abccecca74bd090cb44d7
tee <tee@inspirion>
2024-12-15 17:43:19 +0100

    1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.app.Gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+	requires org.apache.cxf.tools.wsdl.core;
+	requires jakarta.xml.ws;
+	requires javafx.graphics;
+	requires java.logging;
+	exports org.app.Gui;
+}
--- src/main/java/org/bin/run.java
+++ src/main/java/org/bin/run.java
@@ -8,7 +8,6 @@ public class run{
 	
 	public static void main(String[] args) {
 		Gui gui = new Gui();
-		gui.run();
-		
+		gui.run();		
 	}
 }
